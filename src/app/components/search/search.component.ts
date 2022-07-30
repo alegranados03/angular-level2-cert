b063192d631348ad7757544b311b8835f08b7af5
Alejandro Granados <alegranados03@gmail.com>
2022-07-30 13:48:49 -0600

    install and setup bootstrap

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,5 +1,5 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -8,7 +8,10 @@ import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class SearchComponent {
   @Output() searchEvent = new EventEmitter<string>();
   searchForm: FormGroup = this.fb.group({
-    symbol: ['', Validators.required],
+    symbol: [
+      '',
+      [Validators.required, Validators.maxLength(5), Validators.minLength(1)],
+    ],
   });
   constructor(private fb: FormBuilder) {}
 
@@ -21,7 +24,7 @@ export class SearchComponent {
 
   onSearch() {
     if (this.searchForm.valid)
-      this.searchEvent.emit(this.searchForm.value.symbol);
+      this.searchEvent.emit(this.searchForm.value.symbol.trim());
     this.searchForm.reset();
   }
 }
